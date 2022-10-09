301b1db8e92259e85739c2d7114fb3188f961934
TehIvan <83208329+TehIvan@users.noreply.github.com>
2022-10-09 22:22:40 +0200

    Update Types.ts

--- src/classes/Types/Types.ts
+++ src/classes/Types/Types.ts
@@ -1,7 +1,66 @@
 export interface Invoice {
     detail: InvoiceDetail;
     additional_recipients?: EmailAddress[];
-    amount: AmountSummaryDetail;
+    amount?: AmountSummaryDetail;
+    configuration?: Configuration;
+    due_amount?: Money;
+    gratuity?: Money;
+    id?: string;
+    invoicer?: InvoicerInfo;
+    items?: Item[];
+    links?: LinkDescription[];
+}
+
+export interface LinkDescription {
+    href: string;
+    rel: string;
+    method?: LinkMethod;
+}
+
+export type LinkMethod = "GET"|"POST"|"PUT"|"DELETE"|"HEAD"|"CONNECT"|"OPTIONS"|"PATCH"
+
+export interface Item {
+    name: string;
+    quantity: string;
+    unit_amount: Money;
+    discount?: Discount;
+    id?: string;
+    item_date: string;
+    tax?: Tax;
+    unit_of_measure?: UnitOfMeasure;
+}
+
+export type UnitOfMeasure = "QUANTITY" | "HOURS" | "AMOUNT";
+
+export interface InvoicerInfo {
+    additional_notes?: string;
+    email_address?: string;
+    logo_url?: string;
+    phones?: PhoneDetail[]
+    tax_id?: string;
+    website?: string;
+}
+
+export interface PhoneDetail {
+    country_code: string;
+    national_number: string;
+    extension_number?: string;
+    phone_type?: PhoneType;
+}
+
+export type PhoneType = "FAX"|"HOME"|"MOBILE"|"OTHER"|"PAGER";
+
+export interface Configuration {
+    allow_tip?: boolean;
+    partial_payment?: PartialPayment;
+    tax_calculated_after_discount?: boolean;
+    tax_inclusive?: boolean;
+    template_id?: string;
+}
+
+export interface PartialPayment {
+    allow_partial_payment?: boolean;
+    minimum_amount_due?: Money;
 }
 
 export interface InvoiceDetail {
